3178885fb84dc5c7523005ce8d83362b4605d4c3
dongspam0209 <98256216+dongspam0209@users.noreply.github.com>
2024-04-02 12:30:25 +0900

    [level 2] Title: 타겟 넘버, Time: 2.59 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,43 +1,7 @@
-from collections import defaultdict
-global path
-path=list()
-
+from itertools import product
 def solution(numbers, target):
-    answer = 0
-    numbers_with_sign=list()
-    number_graph=defaultdict(list)
-    
-    for number in numbers:
-        pos_neg=[number,-number]
-        numbers_with_sign.extend(pos_neg)
-        
-    for idx in range(0,len(numbers_with_sign)-2,2):
-        idx_list=[idx+2,idx+3]
-        number_graph[idx].extend(idx_list)
-        number_graph[idx+1].extend(idx_list)
-    
-    DFS(number_graph,0,len(numbers_with_sign)-2,numbers_with_sign,target)
-    DFS(number_graph,0,len(numbers_with_sign)-1,numbers_with_sign,target)
-    DFS(number_graph,1,len(numbers_with_sign)-2,numbers_with_sign,target)
-    DFS(number_graph,1,len(numbers_with_sign)-1,numbers_with_sign,target)
-    
-    answer=len(path)
+    num_with_sign=[(x,-x)for x in numbers]
+    all_case= list(map(sum,product(*num_with_sign)))
     
     
-    return answer
-
-def DFS(graph,start,end,nums_with_sign,target,visited=[]):
-    visited=visited+[start]
-    if start==end:
-        sum_list=list()
-        for idx in visited:
-            sum_list.append(nums_with_sign[idx])
-        if target==sum(sum_list):
-            path.append(visited)
-        
-    for node in graph[start]:
-        if node not in visited:
-            DFS(graph,node,end,nums_with_sign,target,visited)
-            
-
-    return path
+    return all_case.count(target)
