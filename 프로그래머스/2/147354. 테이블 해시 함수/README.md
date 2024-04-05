e8bb376dfd3c1a876d358e5552eba90b844e1334
dongspam0209 <98256216+dongspam0209@users.noreply.github.com>
2024-04-06 01:00:55 +0900

    [level 2] Title: 테이블 해시 함수, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/147354. 테이블 해시 함수/README.md
+++ 프로그래머스/2/147354. 테이블 해시 함수/README.md
@@ -1,10 +1,10 @@
 # [level 2] 테이블 해시 함수 - 147354 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/147354) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/147354#) 
 
 ### 성능 요약
 
-메모리: 10.3 MB, 시간: 0.01 ms
+메모리: 10.2 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 06일 00:51:35
+2024년 04월 06일 01:00:53
 
 ### 문제 설명
 
--- 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.py
+++ 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.py
@@ -1,12 +1,13 @@
 def solution(data, col, row_begin, row_end):
-    data.sort(key=lambda x: (x[col - 1], -x[0]))
-    rows = zip(range(row_begin, row_end+1), data[row_begin - 1: row_end])
-
-    answer = 0
-    for idx, row in rows:
-        _sum = 0
-        for element in row:
-            _sum += element % idx
-        answer ^= _sum
-
+    data.sort(key=lambda x : (x[col-1],-x[0]))
+    answer=0
+    
+    for idx in range(row_begin-1,row_end):
+        S=0
+        for j in range(len(data[idx])):
+            S+=data[idx][j]%(idx+1)
+        answer^=S
+    
     return answer
+
+    
